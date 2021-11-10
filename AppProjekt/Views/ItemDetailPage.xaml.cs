f34444653cac27f0d0891d191f82bc016051118f
gamingAmee <amalie2410@gmail.com>
2021-11-10 12:02:29 +0100

    Add project files.

---
+++ AppProjekt/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AppProjekt.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppProjekt.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ AppProjekt/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppProjekt.Models;
+using AppProjekt.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppProjekt.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
