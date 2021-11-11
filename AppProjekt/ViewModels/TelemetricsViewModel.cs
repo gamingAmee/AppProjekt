dd3433c967aa930ac66f675e4bd0468de57b5f0f
gamingAmee <amalie2410@gmail.com>
2021-11-11 13:35:09 +0100

    display of microcharts works

--- AppProjekt/ViewModels/TelemetricsViewModel.cs
+++ AppProjekt/ViewModels/TelemetricsViewModel.cs
@@ -14,11 +14,37 @@ namespace AppProjekt.ViewModels
     public class TelemetricsViewModel : BaseViewModel
     {
         public ObservableCollection<Telemetrics> Telemetrics { get; }
+        private ObservableCollection<ChartEntry> Entries = new ObservableCollection<ChartEntry>();
+        public List<string> TimePick
+        {
+            get
+            {
+                return new List<string> { "1 min", "10 mins", "20 mins" };
+            }
+        }
+
+        private string _selectedTime;
+
+        public string SelectedTime
+        {
+            get { return _selectedTime; }
+            set
+            {
+                SetProperty(ref _selectedTime, value);
+
+            }
+        }
+
+
+        private Chart tempChart;
+        public Chart TempChart { get { return tempChart; } set { SetProperty(ref tempChart, value); } }
+
+
         public Command LoadItemsCommand { get; }
 
         public TelemetricsViewModel()
         {
-            Title = "Browse";
+            Title = "Telemetrics";
             Telemetrics = new ObservableCollection<Telemetrics>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
         }
@@ -36,6 +62,7 @@ namespace AppProjekt.ViewModels
                 {
                     Telemetrics.Add(item);
                 }
+                CreateTempChart();
             }
             catch (Exception ex)
             {
@@ -47,20 +74,48 @@ namespace AppProjekt.ViewModels
             }
         }
 
+        public void CreateTempChart()
+        {
+            foreach (var item in Telemetrics.OrderByDescending(t => t.Timestamp).Take(10))
+            {
+                var test = Convert.ToDecimal(item.Temperatur);
+                var entry = new ChartEntry((float)test)
+                {
+                    Label = item.Timestamp.ToString(),
+                    ValueLabel = item.Temperatur,
+                };
+                Entries.Add(entry);
+            }
+            TempChart = new LineChart()
+            {
+                Entries = Entries,
+                PointSize = 10,
+                LineSize = 10,
+                LabelOrientation = Orientation.Horizontal,
+                ValueLabelOrientation = Orientation.Horizontal,
+                LabelTextSize = 30
+            };
+        }
+
+        //void UpdateTempChart(string time)
+        //{
+        //    Entries.Clear();
+        //    foreach (var item in Telemetrics.Where(t => t.Timestamp.Minute == 60))
+        //    {
+        //        var test = Convert.ToDecimal(item.Temperatur);
+        //        var entry = new ChartEntry((float)test)
+        //        {
+        //            Label = item.Timestamp.ToString(),
+        //            ValueLabel = item.Temperatur,
+        //        };
+        //        Entries.Add(entry);
+        //    }
+        //}
+
         public void OnAppearing()
         {
             IsBusy = true;
         }
-
-        //public Chart TempChart
-        //{
-
-        //    get
-        //    {
-        //        var entries = Telemetrics.Select(p => p.Temperatur).AsEnumerable();
-        //        return new LineChart { Entries = entries };
-        //    }
-
-        //}
     }
 }
+
